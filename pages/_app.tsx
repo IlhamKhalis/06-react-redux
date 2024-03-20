05873647df54e3923d6920f501ee657a8d5bd5ab
Ilham Khalis Putra Agatha <khalisagatha@gmail.com>
2024-03-20 13:36:38 +0700

    Praktikum-01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
